2169a8381a1b5b5431d15bde82c29603e68a954e
Panda248 <40081083+Panda248@users.noreply.github.com>
2021-11-23 15:51:02 -0500

    Add files via upload

---
+++ Drive1/Drive1/include/robot-config.h
@@ -0,0 +1,23 @@
+using namespace vex;
+
+extern brain Brain;
+
+// VEXcode devices
+extern controller Controller1;
+extern motor LF;
+extern motor LB;
+extern motor RF;
+extern motor RB;
+extern motor intake1;
+extern motor arm;
+extern inertial Inertial;
+extern motor tilter;
+extern limit bump;
+extern motor hindTilter;
+
+/**
+ * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
+ * 
+ * This should be called at the start of your int main function.
+ */
+void  vexcodeInit( void );
